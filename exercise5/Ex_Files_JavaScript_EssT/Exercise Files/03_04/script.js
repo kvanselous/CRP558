123a46cb3f14444ece73471d76f029b993f3c1cc
kvanselous <kvansel@iastate.edu>
2020-03-03 06:15:08 -0600

    updated exercise 8
    updated

---
+++ exercise5/Ex_Files_JavaScript_EssT/Exercise Files/03_04/script.js
@@ -0,0 +1,5 @@
+var a = 5;
+var b = 4;
+var sum = a + b;
+var humanSum ="The value of a;" +a + ".The value of b; "+b+". The sum of a a and b;"; sum +".";
+console.log(humanSum_;)
---
+++ exercise5/Ex_Files_JavaScript_EssT/Exercise Files/03_07/script.js
@@ -0,0 +1,5 @@
+van pens;
+pens = ["red", "blue", "green", "orange"];
+pens = new Array ("red", "blue", "green", "orange];")
+mixed bag=
+console.log(pens);
